8140300eb7dfdccf1d0fd7bba6a75d4283e9666b
Steven Starnes <starnes81@gmail.com>
2017-11-25 19:57:06 -0600

    added styling and checkmark

--- Todo/script.js
+++ Todo/script.js
@@ -1,3 +1,10 @@
+var completed = document.querySelector('ul');
+completed.addEventListener('click',function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
 function addItem() {
   var newLi = document.createElement("li");
   var inputValue = document.getElementById("tdInput").value;
